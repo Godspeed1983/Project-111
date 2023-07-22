42acfcf3e138f05503ffe3ec3d980423ab9d76e5
Godspeed1983 <109015446+Godspeed1983@users.noreply.github.com>
2023-07-22 13:36:45 +0530

    Add files via upload

---
+++ Project 111/main.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+
+from_dir="C:/Users/Innoc/Downloads/Project 111/testA/file.pdf"
+
+to_dir="C:/Users/Innoc/Downloads/Project 111/testB"
+a=shutil.move(from_dir,to_dir)
+
+
+
+
